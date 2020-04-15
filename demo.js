7284c716e49bb6ef0c42573b12fc8723718df124
pokemon3636 <slgu@cybozu.cn>
2020-04-15 13:43:34 +0800

    bug fix

--- demo.js
+++ demo.js
@@ -7,6 +7,7 @@ https://github.com/ushiront/maptomo-kinportal
 jQuery.noConflict();
 (function ($) {
     'use strict';
+    let a;
 
     // 各个tab的应用配置 --------------------------------------------
 
